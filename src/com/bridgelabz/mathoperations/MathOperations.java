fecd96aa973546d35dbea4feb45801b0f3869d7a
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-16 12:50:58 +0530

    Added : Method which accept lambda function as parameter

--- src/com/bridgelabz/mathoperations/MathOperations.java
+++ src/com/bridgelabz/mathoperations/MathOperations.java
@@ -5,6 +5,10 @@ package com.bridgelabz.mathoperations;
 interface MyMathFuntion
 {
 	int calculate(int a , int b);
+	static void printResult(String operation,int a, int b, MyMathFuntion function)
+	{
+		System.out.println("Result of "+a+" "+operation+" "+b+" is "+function.calculate(a,b));
+	}
 }
 
 public class MathOperations 
@@ -18,9 +22,17 @@ public class MathOperations
 		MyMathFuntion Division = (x,y) -> (x/y);
 		
 		//calling and printing values returned by functions
+		System.out.println("Result using lambda functions");
 		System.out.println("Addition is "+ add.calculate(10, 5));
 		System.out.println("Subtract is "+ Subtract.calculate(10, 5));
 		System.out.println("Multiply is "+ Multiply.calculate(10, 5));
 		System.out.println("Divide is "+ Division.calculate(10, 5));
+		
+		//passing lambda function as function parameter
+		System.out.println("\nResult by lambda functions as function parameter");
+		MyMathFuntion.printResult("Addition", 10, 5, add);
+		MyMathFuntion.printResult("Subtract", 10, 5, Subtract);
+		MyMathFuntion.printResult("Multiply", 10, 5, Multiply);
+		MyMathFuntion.printResult("Divide", 10, 5, Division);
 	}
 }
