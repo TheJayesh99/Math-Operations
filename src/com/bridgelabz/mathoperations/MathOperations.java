0efc74961d2686c577a7e0a72821b722740422fb
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-16 12:35:49 +0530

    Added : Methods to calculate math operation using lambda function

--- src/com/bridgelabz/mathoperations/MathOperations.java
+++ src/com/bridgelabz/mathoperations/MathOperations.java
@@ -1,5 +1,26 @@
 package com.bridgelabz.mathoperations;
 
-public class MathOperations {
+//added functional interface which has 1 abstract method calculate
+@FunctionalInterface
+interface MyMathFuntion
+{
+	int calculate(int a , int b);
+}
 
+public class MathOperations 
+{
+	public static void main(String[] args) 
+	{
+		//defining some lambda function to perform math operations
+		MyMathFuntion add = Integer::sum;
+		MyMathFuntion Subtract = (x,y) -> (x-y);
+		MyMathFuntion Multiply = (x,y) -> (x*y);
+		MyMathFuntion Division = (x,y) -> (x/y);
+		
+		//calling and printing values returned by functions
+		System.out.println("Addition is "+ add.calculate(10, 5));
+		System.out.println("Subtract is "+ Subtract.calculate(10, 5));
+		System.out.println("Multiply is "+ Multiply.calculate(10, 5));
+		System.out.println("Divide is "+ Division.calculate(10, 5));
+	}
 }
